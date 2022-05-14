9c163e3babddedd8406651c152aa4dd8f4980663
Carlos Montaño <81936323+carlos-montano-hub@users.noreply.github.com>
2022-05-13 23:09:04 -0700

    Add files via upload
    Addition of commands.

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from ReadData import readData
 from Util import *
 from ClassVar import *
 from Register import *
-
+import click
 
 def printAllRawData():
     for date in Ledger.transactionsDictionaries:
@@ -12,8 +12,22 @@ def printAllRawData():
         for concept in Ledger.transactionsDictionaries[date]:
             print(concept, Ledger.transactionsDictionaries[date][concept])
 
+@click.command()
+@click.argument("function" , type= str)
+def chooseFunction(function):
+    if function == "Balance":
+        readData()
+        generateBalance()
+    if function ==  "Register":
+        readData()
+        printRegister()
+    if function ==  "Print":
+        readData()
+        printAll()
+    
+    
+if __name__ == '__main__':
+    chooseFunction()
 
-readData()
-generateBalance()
-printRegister()
-printAll()
+
+
